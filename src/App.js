0a50c98b712b920bf2ed7659798acf014adf13f5
dunsin <ajibabidunsin@gmail.com>
2023-01-03 22:52:32 +0100

    starting the table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-
 import './App.css';
+import { Box } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="container">
+      <Box className="header">
+        <Box className='name'>
+          <h3>Toko</h3>
+          <h3>KelongTong</h3>
+          <p>lorem ipsum</p>
+        </Box>
+        <Box className='invoice'>
+          <h1>Invoice</h1>
+          <p> December 26th 2018</p>
+          <Box className='receiver'>
+            <p>To.</p>
+            <h4>Mrs Margreth</h4>
+          </Box>
+          <Box className='details'>
+            <p>Company Name</p>
+            <p>Address and City Name</p>
+            <p>2424 Country Name</p>
+          </Box> 
+        </Box>
+        <Box className='isn'>
+          <h4>NO/ISN 01.12.2019</h4>
+        </Box>
+      </Box>
+      <Box className=''>
+        <table>
+          <tr>
+            <th>QTY</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Total</th>
+          </tr>
+        </table>
+
+      </Box>
+      
+    </Box>
   );
 }
 
